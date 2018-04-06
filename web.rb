aeb3162c55c353e32271f80e3a25b0fdf04808b2
Bryce Kerley <bkerley@brycekerley.net>
2018-04-05 22:02:47 -0400

    fix name of scope field

--- web.rb
+++ web.rb
@@ -14,7 +14,7 @@ post '/step1' do
   q = { response_type: 'code',
         client_id: params[:client_key],
         redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-        scopes: s.join(' ')
+        scope: s.join(' ')
       }
   redirect_uri.query = q.map do |k, v|
     "#{CGI.escape k.to_s}=#{CGI.escape v}"
